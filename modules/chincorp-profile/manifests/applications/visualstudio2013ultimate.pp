718288162a8182288639ddee821b886e7251da1a
Jason Chin <jason.chin@smsmt.com>
2014-12-22 11:21:27 +1100

    Added Visual Studio 2013 ultimate Puppet profile

--- modules/chincorp-profile/manifests/applications/visualstudio2013ultimate.pp
+++ modules/chincorp-profile/manifests/applications/visualstudio2013ultimate.pp
@@ -1,7 +1,15 @@
 class chincorp-profile::applications::visualstudio2013ultimate () inherits chincorp-profile::global_variables {
 
-    package { "cmder":
+    package { "visualstudio2013ultimate":
         ensure => installed,
         provider => chocolatey,
+        source => 'C:\vagrant_data\vs2013'
+    }
+
+    package { "vs2013.4":
+        ensure => installed,
+        provider => chocolatey,
+        source => 'C:\vagrant_data\vs2013.4',
+        require => Package['visualstudio2013ultimate'],
     }
 }
